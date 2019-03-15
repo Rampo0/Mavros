973028ea00ea1176ab5c731c0658e2bf2c4a46e5
Ammar Dwi Anwari <36811859+Rampo0@users.noreply.github.com>
2019-03-16 03:06:39 +0700

    Add files via upload

---
+++ BasicMavrosCmdService.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import NavSatFix
+from mavros_msgs.srv import *
+
+rospy.init_node('BasicCmdServices', anonymous=True)
+#http://wiki.ros.org/mavros/CustomModes for search your custom modes
+
+def setGuidedMode():
+    rospy.wait_for_service('/mavros/set_mode')
+    try:
+        flightModeService = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)
+        isModeChanged = flightModeService(custom_mode='GUIDED')
+    except (rospy.ServiceException, e):
+        print ("service set_mode call failed: %s. GUIDED Mode could not be set. Check that GPS is enabled"%e)
+        
+def setStabilizeMode():
+    rospy.wait_for_service('/mavros/set_mode')
+    try:
+        flightModeService = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)
+        isModeChanged = flightModeService(custom_mode='STABILIZE')
+    except (rospy.ServiceException, e):
+        print ("service set_mode call failed: %s. GUIDED Mode could not be set. Check that GPS is enabled"%e)
+
+def setLandMode():
+    rospy.wait_for_service('/mavros/cmd/land')
+    try:
+        landService = rospy.ServiceProxy('/mavros/cmd/land', mavros_msgs.srv.CommandTOL)
+        isLanding = landService(altitude = 0, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
+    except (rospy.ServiceException, e):
+        print ("service land call failed: %s. The vehicle cannot land "%e)
+          
+def setArm():
+    rospy.wait_for_service('/mavros/cmd/arming')
+    try:
+        armService = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
+        armService(True)
+    except (rospy.ServiceException, e):
+        print ("Service arm call failed: %s"%e)
+        
+def setDisarm():
+    rospy.wait_for_service('/mavros/cmd/arming')
+    try:
+        armService = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
+        armService(False)
+    except (rospy.ServiceException, e):
+        print ("Service arm call failed: %s"%e)
+
+
+def setTakeoffMode():
+    rospy.wait_for_service('/mavros/cmd/takeoff')
+    try:
+        takeoffService = rospy.ServiceProxy('/mavros/cmd/takeoff', mavros_msgs.srv.CommandTOL) 
+        takeoffService(altitude = 2, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
+    except (rospy.ServiceException, e):
+        print ("Service takeoff call failed: %s"%e)
+
+#do code here
+
---
+++ BasicSubscriber.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import NavSatFix
+
+def trackGPSposition(positionCallback):
+    latitude = positionCallback.latitude
+    longitude = positionCallback.longitude
+    print ("longitude: %.7f" %longitude)
+    print ("latitude: %.7f" %latitude)
+
+if __name__ == '__main__':
+    
+    while True:
+        
+        rospy.init_node('BasicSubscriber', anonymous=True)
+        rospy.Subscriber("/mavros/global_position/raw/fix", NavSatFix, positionCallback)
+        trackGPSposition(positionCallback)
+        
+    
